3ce9e552b0575751d9875444b42a421270b375d4
Vasu Nagori <vasu.nagori@incedoinc.com>
2023-04-07 10:23:23 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+using FileController1;
+
+namespace   HandlingFiles
+{
+        public  class HandlingFiles
+        {
+                public static void Main(string[] args)
+                {
+                        Console.WriteLine("Hello, World!");
+                        string FileName = "C:\\Users\\vasu.nagori\\Documents\\Projects\\ChatGPT_Hackathon\\Project_Documents";// Path location of Use_Case File in the Local Machine;
+                                
+                        ExcelController fs = new ExcelController();
+                        List<string> fileContents = fs.ReadFromExcel(FileName); //var fileContents
+                        Console.WriteLine(string.Join("\t",fileContents));
+                }
+        }
+}
